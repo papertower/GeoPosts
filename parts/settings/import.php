a928bd45d8f4225afbdf6297c98f3021ba05eeea
Jason Adams <jason.the.adams@gmail.com>
2017-07-10 16:26:31 -0700

    improved import instructions

--- parts/settings/import.php
+++ parts/settings/import.php
@@ -6,6 +6,9 @@
  * Flow: Settings Workflow
  */
 
+$required_columns = implode(', ', array_merge(GeoPostImport::get_keys('post'), GeoPostImport::get_keys('meta'), GeoPostImport::get_keys('extra')));
+$optional_columns = implode(', ', array_merge(GeoPostImport::get_keys('post', false), GeoPostImport::get_keys('meta', false), GeoPostImport::get_keys('extra', false)));
+
 echo <<<HTML
 <style>
   .bs-callout {
@@ -29,8 +32,12 @@ echo <<<HTML
 </style>
 <div class="bs-callout">
   <h4>Note:</h4>
-  <p>Importing can take a while, especially if coordinates are being retrieved and the list is long. Also <strong>be careful</strong> of the google geocode usage limit (is 2000/day at this point).</p>
-  <p>The best way to import is to first export and then reuse the structure provided.</p>
+  <p>Importing can take a while, especially if coordinates are being retrieved and the list is long. Also <strong>be careful</strong> of the google geocode usage limit (2,500/day, last checked).</p>
+</div>
+<div class="bs-callout">
+  <h4>Columns:</h4>
+  <p>Required Columns: $required_columns</p>
+  <p>Optional Columns: $optional_columns</p>
   <p>The ID column is a special column. If omitted, it will be assumed that all posts are new. If included, the post with the provided ID will be updated. It is acceptable for some posts to have an ID and not others in the same file.</p>
 </div>
 HTML;
